6e91b08e792070f22699af14c166f7b2703b0d34
Isaack Motanya <isaackmotanya6@gmail.com>
2021-01-28 00:29:23 +0300

    Pending push

---
+++ app/Events/MessageSentEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSentEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Message $message
+     * @param User $user
+     */
+    public function __construct(Message $message, User $user)
+    {
+        $this->message = $message;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat');
+    }
+}
